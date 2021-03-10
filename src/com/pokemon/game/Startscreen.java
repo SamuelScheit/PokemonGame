de57a442b7efb99754f2f5951f17a161ba9699a3
Philipp <_p.schmid@gmx.de>
2021-03-10 09:14:52 +0100

    Startscreen class Screen resolution 1280x720

---
+++ src/com/pokemon/game/Startscreen.java
@@ -0,0 +1,25 @@
+package com.pokemon.game;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.graphics.TextRenderer;
+import de.gurkenlabs.litiengine.gui.screens.GameScreen;
+import de.gurkenlabs.litiengine.gui.screens.Resolution;
+
+import java.awt.*;
+
+public class Startscreen extends GameScreen {
+    public Startscreen() {
+        super("SC");
+    }
+
+    @Override
+    public void render(final Graphics2D g) {
+        super.render(g);
+
+        g.setColor(Color.RED);
+        Game.graphics().renderText(g, "my text", 0,0);
+
+    }
+
+
+}
