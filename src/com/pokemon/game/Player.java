a7d829963d872f03569de7e8961eda234d3f7b26
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-03-17 09:00:57 +0100

    fix screens + font size

--- src/com/pokemon/game/Player.java
+++ src/com/pokemon/game/Player.java
@@ -7,9 +7,9 @@ import de.gurkenlabs.litiengine.physics.IMovementController;
 
 import java.awt.event.KeyEvent;
 
-@EntityInfo(width = 18, height = 18)
+@EntityInfo(width = 32, height = 43)
 @MovementInfo(velocity = 70)
-@CollisionInfo(collisionBoxWidth = 8, collisionBoxHeight = 16, collision = true)
+@CollisionInfo(collisionBoxWidth = 32, collisionBoxHeight = 43, collision = true)
 public class Player extends Creature implements IUpdateable {
 
     private static Player instance;
@@ -23,7 +23,7 @@ public class Player extends Creature implements IUpdateable {
     }
 
     private Player() {
-        super("gurknukem");
+        super("character");
         // setup movement controller
         KeyboardEntityController<Player> movementController = new KeyboardEntityController<>(this);
         movementController.addUpKey(KeyEvent.VK_UP);
