5f8bb2fb11bb3b15c51bbcdcd82b6b563f2e43d5
Colin <ckm.kunzmann@gmail.com>
2021-03-24 14:19:21 +0100

    Gui SC 2

--- src/com/pokemon/game/Pausescreen.java
+++ src/com/pokemon/game/Pausescreen.java
@@ -9,18 +9,15 @@ import java.awt.*;
 public class Pausescreen extends GameScreen {
     public Pausescreen() {
         super("PAUSE");
-    }
 
-    public static class GUI {
+}
+public static class GUI {
         private JPanel panel1;
         private JButton quitGameButton;
         private JButton returnToHomeButton;
         private JButton settingsButton;
         private JButton resumeButton;
-        private JTextField PAUSETextField;
-
-        private void createUIComponents() {
-            // TODO: place custom component creation code here
-        }
-    }
-
+        private JTextField PAUSE;
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+}
