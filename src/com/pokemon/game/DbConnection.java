fabed848783cdbed968e165042ea19485b82010f
Philipp <_p.schmid@gmx.de>
2021-04-10 13:13:41 +0200

    Database integration

---
+++ src/com/pokemon/game/DbConnection.java
@@ -0,0 +1,21 @@
+package com.pokemon.game;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    public static Connection connect(){
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:GameDatabase.db");
+            System.out.println("Connected");
+
+        } catch(Exception e){
+                System.out.println(e + "");
+            }
+            return c;
+
+    }
+}
--- src/com/pokemon/game/Main.java
+++ src/com/pokemon/game/Main.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        DbConnection.connect();
         Game.setInfo("assets/gameinfo.xml");
         Game.init(args);
         Resources.load("assets/game.litidata");
