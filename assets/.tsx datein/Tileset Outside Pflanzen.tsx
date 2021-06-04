1618f929b9856707916daf0b1d1a1c9d719bd0a2
Lichtkrieger <poppfelix@icloud.com>
2021-06-04 22:36:04 +0200

    Alle maps vorerst final

--- assets/.tsx datein/Tileset Outside Pflanzen.tsx
+++ assets/.tsx datein/Tileset Outside Pflanzen.tsx
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Tileset Outside Pflanzen" tilewidth="16" tileheight="16" tilecount="600" columns="6">
+<tileset version="1.5" tiledversion="1.6.0" name="Tileset Outside Pflanzen" tilewidth="16" tileheight="16" tilecount="600" columns="6">
  <image source="../Tileset Outside Pflanzen.png" width="96" height="1600"/>
  <tile id="162">
   <animation>
-   <frame tileid="162" duration="150"/>
-   <frame tileid="163" duration="150"/>
-   <frame tileid="164" duration="150"/>
-   <frame tileid="165" duration="150"/>
+   <frame tileid="162" duration="250"/>
+   <frame tileid="163" duration="250"/>
+   <frame tileid="164" duration="250"/>
+   <frame tileid="165" duration="250"/>
   </animation>
  </tile>
  <tile id="168">
